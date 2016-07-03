76bea803c8d66351a3f5530c261f91466055e00c
root <alessandro.cpr@gmail.com>
2016-07-03 11:55:53 +0200

    Fixata favicon

--- php/getMacro.php
+++ php/getMacro.php
@@ -6,7 +6,7 @@ if (isset($_POST['id'])) {
 
     $id=$_POST['id'];
 
-    $sql = "SELECT id,nome FROM Macro WHERE mega_id = $id";
+    $sql = "SELECT id,nome FROM Macro WHERE mega_id = $id and id NOT IN (11,13,16,45,77,92,96,189,201,211,227)";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
